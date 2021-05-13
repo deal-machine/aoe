749fcb6365eec46b47e7213ab0cc0d6007feebb0
deal <95deal@gmail.com>
2021-05-13 17:26:42 -0300

    started project

---
+++ src/main/java/br/com/dealmachine/mvc/aoe/AoeApplication.java
@@ -0,0 +1,13 @@
+package br.com.dealmachine.mvc.aoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dealmachine/mvc/aoe/AoeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dealmachine.mvc.aoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
