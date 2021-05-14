555f6ad6418edb46f787c121977d3777eaa67e54
deal <95deal@gmail.com>
2021-05-14 15:35:33 -0300

    created first  OrderArt

--- src/main/java/br/com/dealmachine/mvc/aoe/controller/HomeController.java
+++ src/main/java/br/com/dealmachine/mvc/aoe/controller/HomeController.java
@@ -1,34 +1,33 @@
 package br.com.dealmachine.mvc.aoe.controller;
 
 import java.util.List;
-import java.util.Arrays;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import br.com.dealmachine.mvc.aoe.model.Order;
+import br.com.dealmachine.mvc.aoe.model.OrderArt;
 
 @Controller
 public class HomeController {
-
+	
+	@PersistenceContext
+	private EntityManager EntityManager;
+	
 	
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		Order order = new Order();
+		Query query = EntityManager.createQuery("select p from OrderArt p", OrderArt.class);
+		List<OrderArt> orderArts = query.getResultList();
 		
-		order.setName("Notebook Acer");
-		order.setUrlImage("https://www.alura.com.br/assets/api/cursos/spring-mvc-thymeleaf-bootstrap.svg");
-		order.setUrlProduct("https://static.javatpoint.com/images/logo/jtp_logo.png");
-		order.setValue(15.10);	
-		order.setDateDelivery("2021-05-14");
-		order.setDescription("Description of this images and products");
 		
-		List<Order> orders = Arrays.asList(order);
 		
-		model.addAttribute("orders", orders);
+		model.addAttribute("orders", orderArts);
 		
 		return "home";
 	}
---
+++ src/main/java/br/com/dealmachine/mvc/aoe/model/OrderArt.java
@@ -0,0 +1,76 @@
+package br.com.dealmachine.mvc.aoe.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class OrderArt {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private BigDecimal value;
+	private LocalDate dateDelivery;
+	private String urlProduct;
+	private String urlImage;
+	private String description;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateDelivery == null) ? 0 : dateDelivery.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((urlImage == null) ? 0 : urlImage.hashCode());
+		result = prime * result + ((urlProduct == null) ? 0 : urlProduct.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getValue() {
+		return value;
+	}
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	public LocalDate getDateDelivery() {
+		return dateDelivery;
+	}
+	public void setDateDelivery(LocalDate dateDelivery) {
+		this.dateDelivery = dateDelivery;
+	}
+	public String getUrlProduct() {
+		return urlProduct;
+	}
+	public void setUrlProduct(String urlProduct) {
+		this.urlProduct = urlProduct;
+	}
+	public String getUrlImage() {
+		return urlImage;
+	}
+	public void setUrlImage(String urlImage) {
+		this.urlImage = urlImage;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
