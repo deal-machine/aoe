37555dac5b4f60d9829f6dce7891836e51c0fbd9
deal <95deal@gmail.com>
2021-05-18 15:41:49 -0300

    jdbc authentication / bcrypt

--- src/main/java/br/com/dealmachine/mvc/aoe/WebSecurityConfig.java
+++ src/main/java/br/com/dealmachine/mvc/aoe/WebSecurityConfig.java
@@ -1,19 +1,26 @@
 package br.com.dealmachine.mvc.aoe;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private DataSource dataSource;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -22,22 +29,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.authorizeRequests()
 				.anyRequest().authenticated()
 			.and()
-				.formLogin(form -> form.loginPage("/login").permitAll())
+				.formLogin(form -> form
+						.loginPage("/login")
+						.defaultSuccessUrl("/home", true)
+						.permitAll()
+				)
 				.logout(logout -> logout.logoutSuccessUrl("/logout"));
 	}
 	
-	@Bean
 	@Override
-	public UserDetailsService userDetailsService() {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
-		//authentication
-		UserDetails user = 
-				User.withDefaultPasswordEncoder()
-					.username("95deal")
-					.password("deal")
-					.roles("ADM")
-					.build();
+//		UserDetails user = 
+//				User.builder()
+//					.username("douglas")
+//					.password(encoder.encode("dog"))
+//					.roles("ADM")
+//					.build();
 		
-		return new InMemoryUserDetailsManager(user);
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder);
+//			.withUser(user);
 	}
+	
 }
